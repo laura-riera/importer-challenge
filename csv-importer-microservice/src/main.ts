ae5b926bd6491ec05bd76b36ee20fc1554f9b31f
LRiera24 <laura.riera2412@gmail.com>
2025-06-01 21:57:42 +0200

    Add swagger

--- csv-importer-microservice/src/main.ts
+++ csv-importer-microservice/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('CSV Importer API')
+    .setDescription('API to upload and import emissions data from CSV files')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
+
+  await app.listen(3000);
 }
 bootstrap();

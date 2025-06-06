2807390e6e7739a504fb64d4483700d33c7b1250
LRiera24 <laura.riera2412@gmail.com>
2025-06-06 23:22:46 +0200

    Fix transform api

--- query-api-microservice/src/app.module.ts
+++ query-api-microservice/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EmissionsModule } from './emissions/emissions.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EmissionsModule],
 })
 export class AppModule {}
